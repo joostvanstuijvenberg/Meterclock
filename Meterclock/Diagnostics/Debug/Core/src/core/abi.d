b94cd59a2bdec2e99a66d127edcac93414ccc3ae
Joost van Stuijvenberg <software@joostvanstuijvenberg.nl>
2022-02-17 12:34:19 +0100

    Removed stale dir

--- Meterclock/Diagnostics/Debug/Core/src/core/abi.d
+++
@@ -1,7 +0,0 @@
-Core/src/core/abi.d Core/src/core/abi.o: ../Core/src/core/abi.cpp \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
--- Meterclock/Diagnostics/Debug/Core/src/core/hooks.d
+++
@@ -1,1 +0,0 @@
-Core/src/core/hooks.d Core/src/core/hooks.o: ../Core/src/core/hooks.c
